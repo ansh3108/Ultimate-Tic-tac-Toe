f11c264dccd36bf0081251e31b72aae605affc9b
Ansh Kumar <115077985+ansh3108@users.noreply.github.com>
2024-07-27 17:38:07 +0530

    Update board.py

--- board.py
+++ board.py
@@ -29,3 +29,95 @@ class Board:
                 sqr = self.squares[row][col]
                 s += str(sqr)
         return s
+
+    def create_ultimate(self):
+        for row in range(DIM):
+            for col in range(DIM):
+
+                size = self.dims.sqsize
+                xcor, ycor = self.dims.xcor + (col * self.dims.sqsize), self.dims.ycor + (row * self.dims.sqsize)
+                dims = BoardDim(size=size, xcor=xcor, ycor=ycor)
+                linewidth = self.linewidth - 7
+                ultimate = self.max
+
+                self.squares[row][col] = Board(dims=dims, linewidth=linewidth, ultimate=ultimate, max=False)
+    
+    def render(self, surface):
+        for row in range(DIM):
+            for col in range(DIM):
+                sqr = self.squares[row][col]
+
+                if isinstance(sqr, Board): sqr.render(surface)
+        
+        # vertical lines
+        pygame.draw.line(surface, LINE_COLOR, (self.dims.xcor + self.dims.sqsize, self.dims.ycor),                  (self.dims.xcor + self.dims.sqsize, self.dims.ycor + self.dims.size), self.linewidth)
+        pygame.draw.line(surface, LINE_COLOR, (self.dims.xcor + self.dims.size - self.dims.sqsize, self.dims.ycor), (self.dims.xcor + self.dims.size - self.dims.sqsize, self.dims.ycor + self.dims.size), self.linewidth)
+        
+        # horizontal lines
+        pygame.draw.line(surface, LINE_COLOR, (self.dims.xcor, self.dims.ycor + self.dims.sqsize),                  (self.dims.xcor + self.dims.size, self.dims.ycor + self.dims.sqsize), self.linewidth)
+        pygame.draw.line(surface, LINE_COLOR, (self.dims.xcor, self.dims.ycor + self.dims.size - self.dims.sqsize), (self.dims.xcor + self.dims.size, self.dims.ycor + self.dims.size - self.dims.sqsize), self.linewidth)
+
+    def valid_sqr(self, xclick, yclick):
+
+        row = yclick // self.dims.sqsize
+        col = xclick // self.dims.sqsize
+
+        if row > 2: row %= DIM
+        if col > 2: col %= DIM
+
+        sqr = self.squares[row][col]
+
+        if not isinstance(sqr, Board):
+            return sqr == 0 and self.active
+
+        return sqr.valid_sqr(xclick, yclick)
+
+    def mark_sqr(self, xclick, yclick, player):
+        row = yclick // self.dims.sqsize
+        col = xclick // self.dims.sqsize
+
+        if row > 2: row %= DIM
+        if col > 2: col %= DIM
+
+        sqr = self.squares[row][col]
+
+        print('marking -> (', row, col, ')')
+        if not isinstance(sqr, Board):
+            self.squares[row][col] = player
+            return
+
+        sqr.mark_sqr(xclick, yclick, player)
+
+    def draw_fig(self, surface, xclick, yclick):
+        row = yclick // self.dims.sqsize
+        col = xclick // self.dims.sqsize
+
+        if row > 2: row %= DIM
+        if col > 2: col %= DIM
+
+        sqr = self.squares[row][col]
+
+        if not isinstance(sqr, Board):
+
+            if sqr == 1:
+                ipos = (self.dims.xcor + (col * self.dims.sqsize) + self.offset, 
+                        self.dims.ycor + (row * self.dims.sqsize) + self.offset)
+                fpos = (self.dims.xcor + self.dims.sqsize * (1 + col) - self.offset, 
+                        self.dims.ycor + self.dims.sqsize * (1 + row) - self.offset)
+                pygame.draw.line(surface, CROSS_COLOR, ipos, fpos, self.linewidth)
+
+                ipos = (self.dims.xcor + (col * self.dims.sqsize) + self.offset, 
+                        self.dims.ycor + self.dims.sqsize * (1 + row) - self.offset)
+                fpos = (self.dims.xcor + self.dims.sqsize * (1 + col) - self.offset, 
+                        self.dims.ycor + (row * self.dims.sqsize) + self.offset)
+                pygame.draw.line(surface, CROSS_COLOR, ipos, fpos, self.linewidth)
+            
+            elif sqr == 2:
+                center = (self.dims.xcor + self.dims.sqsize * (0.5 + col),
+                          self.dims.ycor + self.dims.sqsize * (0.5 + row))
+
+                pygame.draw.circle(surface, CIRCLE_COLOR, center, self.radius, self.linewidth)
+
+            return
+
+        sqr.draw_fig(surface, xclick, yclick)
